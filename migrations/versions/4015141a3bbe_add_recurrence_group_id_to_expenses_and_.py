e86c8641c4bcdabfd6760ff16d3f32a25240bc6d
tu-nombre <tu-email@example.com>
2025-10-06 12:10:49 +0200

    chore: add missing migration file

---
+++ migrations/versions/4015141a3bbe_add_recurrence_group_id_to_expenses_and_.py
@@ -0,0 +1,25 @@
+"""Add recurrence_group_id to expenses and incomes
+
+Revision ID: 4015141a3bbe
+Revises: cf393d2bdae3
+Create Date: 2025-10-06 08:23:27.568956
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4015141a3bbe'
+down_revision = 'cf393d2bdae3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
